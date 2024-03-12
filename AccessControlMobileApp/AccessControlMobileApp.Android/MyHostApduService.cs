311944b5824534153826660a60e89f1b191d8747
andumihailescu <contact.andumihailescu@gmail.com>
2024-03-12 18:17:48 +0200

    Added LoginPage (not working yet)

---
+++ AccessControlMobileApp/AccessControlMobileApp.Android/MyHostApduService.cs
@@ -0,0 +1,37 @@
+﻿/*using Android.App;
+using Android.Content;
+using Android.Nfc.CardEmulators;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AccessControlMobileApp.Droid
+{
+    [Service(Exported = true, Enabled = true)]
+    [IntentFilter(new[] { "android.nfc.cardemulation.action.HOST_APDU_SERVICE" },
+    Categories = new[] { "android.intent.category.DEFAULT" })]
+    [MetaData("android.nfc.cardemulation.host_apdu_service",
+         Resource = "@xml/apduservice")]
+    public class MyHostApduService : HostApduService
+    {
+        public override byte[] ProcessCommandApdu(byte[] commandApdu, Bundle extras)
+        {
+            var responseApdu;
+            // Handle incoming APDU commands
+            // ...
+
+            // Return the response APDU
+            return responseApdu;
+        }
+
+        public override void OnDeactivated(DeactivationReason reason)
+        {
+            // Handle HCE deactivation
+        }
+    }
+}*/
--- AccessControlMobileApp/AccessControlMobileApp/ViewModels/AboutViewModel.cs
+++ AccessControlMobileApp/AccessControlMobileApp/ViewModels/AboutViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AccessControlMobileApp.Views;
+using System;
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
@@ -7,12 +8,23 @@ namespace AccessControlMobileApp.ViewModels
 {
     public class AboutViewModel : BaseViewModel
     {
+        public Command LgtCommand { get; }
+
         public AboutViewModel()
         {
             Title = "About";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            //LogoutCommand = new Command(async () => await Shell.Current.GoToAsync($"//LoginPage"));
+            LgtCommand = new Command(OnLogoutClicked);
         }
 
         public ICommand OpenWebCommand { get; }
+
+        public ICommand LogoutCommand { get; }
+
+        public async void OnLogoutClicked(object obj)
+        {
+            await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+        }
     }
 }
